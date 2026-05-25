f892c16d89cf5506600315e7cb4c60341bc0e55a
francescacanali <channels4work@gmail.com>
2026-05-25 21:15:34 +0200

    Update export.js

--- api/export.js
+++ api/export.js
@@ -14,17 +14,19 @@ export default async function handler(req, res){
     const rows = await sql`
       SELECT
         id,
-        family_name        AS "Family name",
-        given_name         AS "Given name",
-        gender             AS "Gender",
-        country_birth      AS "Country of Birth",
-        country_residence  AS "Country of Residence",
-        TO_CHAR(date_of_birth,'YYYY-MM-DD') AS "Date of Birth",
-        email              AS "E-mail",
-        phone              AS "Phone",
-        affiliation_code   AS "National affiliation code",
-        photo_url          AS "Photo",
-        chinese_name       AS "Chinese name",
+        TO_CHAR(submitted_at AT TIME ZONE 'UTC','FMMM/FMDD/YYYY FMHH24:MI:SS') AS "Timestamp",
+        email                                  AS "Email Address",
+        family_name                            AS "Family name",
+        given_name                             AS "Given name",
+        gender                                 AS "Gender",
+        country_birth                          AS "Country of Birth",
+        country_residence                      AS "Country of Residence",
+        TO_CHAR(date_of_birth,'FMMM/FMDD/YYYY') AS "Date of Birth",
+        email                                  AS "E-mail",
+        phone                                  AS "Phone",
+        photo_url                              AS "Photo",
+        affiliation_code                       AS "National affiliation code",
+        chinese_name                           AS "Chinese name",
         TO_CHAR(submitted_at AT TIME ZONE 'UTC','YYYY-MM-DD HH24:MI') AS submitted_at
       FROM registrations
       ORDER BY submitted_at DESC
